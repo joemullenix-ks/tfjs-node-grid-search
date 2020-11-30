99a0306fe45728acce1aa5d24c9b255084f5495c
joemullenix-ks <joemullenix@hotmail.com>
2020-11-29 15:32:07 -1000

    Change main to an export-all test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 'use strict';
 
 
-module.exports = class Search {
+// import { FileIOResult } from './lib/FileIOResult';
+
+
+export * from './lib/FileIOResult';
+
+/*
+export declare const FileIOResult;
+
+
+// module.exports = class Search {
+export class Search {
 	constructor() {
-		console.log('instantiated search v00');
+		console.log('instantiated search');
 	}
 }
+*/
