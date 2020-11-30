afe2b2d7a8626330ce109454979d152be6a3fe58
joemullenix-ks <joemullenix@hotmail.com>
2020-11-29 16:02:01 -1000

    Update main exports

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,21 @@
 // import { FileIOResult } from './lib/FileIOResult';
 
 
-export * from './lib/FileIOResult';
+// export * from './lib/FileIOResult';
+
+/*
+AXIS_SET,
+MODEL_STATICS,
+SESSION_DATA,
+EVALUATE_PREDICTION,
+GRID_OPTIONS
+*/
+
+export * from './lib/AxisSet';
+export * from './lib/GridOptions';
+export * from './lib/ModelStatics';
+export * from './lib/SessionData';
+
 
 /*
 export declare const FileIOResult;
