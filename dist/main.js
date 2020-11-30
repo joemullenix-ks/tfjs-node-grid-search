69dfaf799951130e690049eb1246c34a1a443432
joemullenix-ks <joemullenix@hotmail.com>
2020-11-29 15:34:33 -1000

    Rebuild local TS

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,15 @@
 'use strict';
-module.exports = /** @class */ (function () {
-    function Search() {
-        console.log('instantiated search v00');
-    }
-    return Search;
-}());
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// import { FileIOResult } from './lib/FileIOResult';
+__exportStar(require("./lib/FileIOResult"), exports);
 //# sourceMappingURL=main.js.map
