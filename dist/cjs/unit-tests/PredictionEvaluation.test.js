70656bb5e2eb728d55a9f0fcc80f2d464cfd4d4e
joemullenix-ks <joemullenix@hotmail.com>
2020-12-14 13:27:06 -1000

    Add PredictionEvaluation tests

---
+++ dist/cjs/unit-tests/PredictionEvaluation.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const PredictionEvaluation_1 = require("../src/lib/PredictionEvaluation");
+test('instantiation; readonlys', () => {
+    const CORRECT = true;
+    const DELTA = -10;
+    const predictionEvaluation = new PredictionEvaluation_1.PredictionEvaluation(CORRECT, DELTA);
+    expect(predictionEvaluation.correct).toBe(CORRECT);
+    expect(predictionEvaluation.delta).toBe(DELTA);
+});
+//# sourceMappingURL=PredictionEvaluation.test.js.map
