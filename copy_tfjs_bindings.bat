89d0f522f7f7bcf4647092d389927e04791f552f
joemullenix-ks <joemullenix@hotmail.com>
2021-10-08 10:35:51 -1000

    Add dll copy to build workflow

---
+++ copy_tfjs_bindings.bat
@@ -0,0 +1,6 @@
+echo OFF
+
+REM TJFS keeps botching this so that it takes extra steps to friggin use.
+
+copy .\node_modules\@tensorflow\tfjs-node\deps\lib\tensorflow.dll .\node_modules\@tensorflow\tfjs-node\lib\napi-v7\
+REM still? \\?\D:\a\tfjs-node-grid-search\tfjs-node-grid-search\node_modules\@tensorflow\tfjs-node\lib\napi-v7\tfjs_binding.node
